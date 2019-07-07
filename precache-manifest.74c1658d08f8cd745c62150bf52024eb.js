a357c5301676b25a855e14f95499a2ef528bc5ff
Anupriyam <anupriyamranjit8@gmail.com>
2019-07-07 17:32:32 +0545

    Updates

---
+++ precache-manifest.74c1658d08f8cd745c62150bf52024eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b12abfb6637f542fa50",
+    "url": "/PortfolioWebsite/static/css/main.a61930f2.chunk.css"
+  },
+  {
+    "revision": "4b12abfb6637f542fa50",
+    "url": "/PortfolioWebsite/static/js/main.4b12abfb.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/PortfolioWebsite/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "8bbd51d6d62ca8814d98",
+    "url": "/PortfolioWebsite/static/js/runtime~main.8bbd51d6.js"
+  },
+  {
+    "revision": "8c4802b4a1bd66bf21ef1f6f24619747",
+    "url": "/PortfolioWebsite/index.html"
+  }
+];
